a44c5180c2cc9bae24b5fa4f2dc5915b2c228ee7
Kevin Carmona Serrano <kevincarm016@hotmail.com>
2022-09-28 20:21:42 -0500

    User repository fixed, it was not using CrudRepository interface

--- src/main/java/com/kev/jpa/jwt/entities/repository/UserRepository.java
+++ src/main/java/com/kev/jpa/jwt/entities/repository/UserRepository.java
@@ -2,11 +2,12 @@ package com.kev.jpa.jwt.entities.repository;
 
 import com.kev.jpa.jwt.entities.model.User;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends CrudRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE email = :email")
     User getUserByEmail(@Param("email") String email);
 }
