cc411794f85c16991008430943891da5213ee030
Kevin Carmona Serrano <kevincarm016@hotmail.com>
2022-09-28 20:24:58 -0500

    User service implementation created

---
+++ src/main/java/com/kev/jpa/jwt/entities/implementation/UserImplementation.java
@@ -0,0 +1,31 @@
+package com.kev.jpa.jwt.entities.implementation;
+
+import com.kev.jpa.jwt.entities.model.User;
+import com.kev.jpa.jwt.entities.repository.UserRepository;
+import com.kev.jpa.jwt.entities.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserImplementation implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public List<User> getAll() {
+        return (List<User>) repository.findAll();
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return repository.getUserByEmail(email);
+    }
+
+    @Override
+    public User getOneById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+}
