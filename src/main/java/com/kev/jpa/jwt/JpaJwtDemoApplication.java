2ee73c47e5e6112dc94b1c9f9e72dfc4897bf997
Kevin Carmona Serrano <kevincarm016@hotmail.com>
2022-09-28 19:13:24 -0500

    Init commit

---
+++ src/main/java/com/kev/jpa/jwt/JpaJwtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kev.jpa.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaJwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaJwtDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kev/jpa/jwt/JpaJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kev.jpa.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaJwtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
