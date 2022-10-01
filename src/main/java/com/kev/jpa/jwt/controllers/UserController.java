1bf21ef4d3cd863fcbc2128545249562beaa9ebe
Kevin Carmona Serrano <kevincarm016@hotmail.com>
2022-10-01 12:19:31 -0500

    Add user controller with request mapping

---
+++ src/main/java/com/kev/jpa/jwt/controllers/UserController.java
@@ -0,0 +1,48 @@
+package com.kev.jpa.jwt.controllers;
+
+import com.kev.jpa.jwt.entities.model.User;
+import com.kev.jpa.jwt.entities.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/")
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOneById(@PathVariable Long id) {
+        User user = service.getOneById(id);
+        if(user == null) {
+            Map<String, Object> error = new HashMap<>();
+            error.put("code", HttpStatus.NOT_FOUND);
+            error.put("message", "User not found");
+            return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/query")
+    public ResponseEntity<?> getByEmail(@RequestParam("email") String email) {
+        User user = service.getUserByEmail(email);
+        if(user == null) {
+            Map<String, Object> error = new HashMap<>();
+            error.put("code", HttpStatus.NOT_FOUND);
+            error.put("message", "User not found");
+            return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(user);
+    }
+}
