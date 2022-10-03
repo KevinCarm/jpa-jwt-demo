8aa18c64692f1a8253518a726104658e8ec9bb30
Kevin Carmona Serrano <kevincarm016@hotmail.com>
2022-10-02 20:10:39 -0500

    Add user details service

---
+++ src/main/java/com/kev/jpa/jwt/entities/service/MyUserDetailsService.java
@@ -0,0 +1,47 @@
+package com.kev.jpa.jwt.entities.service;
+
+import com.kev.jpa.jwt.entities.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Service that a user with the given email is looking for
+ **/
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserService service;
+
+    /**
+     * Get the user that matches with the email provided
+     *
+     * @param email email provided by the user in the login
+     * @return a new object {@link org.springframework.security.core.userdetails.User} with the email, password and roles
+     */
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = service.getUserByEmail(email);
+        if (user != null) {
+            List<GrantedAuthority> roles;
+            roles = user.getRoles()
+                    .stream()
+                    .map(item -> new SimpleGrantedAuthority(item.getName()))
+                    .collect(Collectors.toList());
+            return new org.springframework.security.core.userdetails.User(
+                    user.getUsername(),
+                    user.getPassword(),
+                    roles
+            );
+        }
+        return null;
+    }
+}
